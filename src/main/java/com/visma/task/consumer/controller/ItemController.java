f2761a9b90046ac79bbc5a7a2ffe97e37b8cece3
kama <kazimieraspovilasmaceika@Kazimierass-MacBook-Pro.local>
2021-06-05 16:30:24 +0300

    tried something with webflux/webclient

--- src/main/java/com/visma/task/consumer/controller/ItemController.java
+++ src/main/java/com/visma/task/consumer/controller/ItemController.java
@@ -1,23 +1,35 @@
 package com.visma.task.consumer.controller;
 
-import com.visma.task.consumer.model.Item;
 import com.visma.task.consumer.model.Status;
 import com.visma.task.consumer.model.StatusType;
 import com.visma.task.consumer.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping(value = "api/items", produces = "application/json")
 public class ItemController {
-    //TODO create controller advice
+
+    private static final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
 
     private ItemService itemService;
 
+    private final WebClient webClient;
+
     @Autowired
-    public ItemController(ItemService itemService) {
+    public ItemController(@Value("${thirdparty.url.base}") String baseUrl, ItemService itemService) {
+        this.webClient = WebClient.builder().baseUrl(baseUrl).build();
         this.itemService = itemService;
     }
 
@@ -33,5 +45,39 @@ public class ItemController {
         } while (!statusType.equals(StatusType.OK));
 
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/v2/{content}")
+    public Mono<ResponseEntity<Status>> getItemStatusFlux(@PathVariable String content) {
+        return getUuidAndStatus().map(e -> ResponseEntity.ok().body(e));
+    }
+
+    private Mono<Status> getUuidAndStatus() {
+        return getUuid().flatMap(this::getOkStatus);
+    }
+
+    private Mono<String> getUuid() {
+        return webClient.post().uri("/init").retrieve()
+                .bodyToMono(String.class)
+                .retryWhen(Retry.backoff(10, Duration.ofSeconds(1)));
+    }
+
+    private Mono<Status> getOkStatus(String uuid) {
+        return getStatus(uuid).flatMap(e -> {
+            //logger.info("New Status for {}: {}", uuid, e);
+            if (e.getStatusType().equals(StatusType.IN_PROGRESS)) {
+                return getOkStatus(uuid);
+            }  if (e.getStatusType().equals(StatusType.FAILED)) {
+                return getUuidAndStatus();
+            } else {
+                return Mono.just(e);
+            }
+        });
+    }
+
+    private Mono<Status> getStatus(String uuid) {
+        return webClient.get().uri("/checkStatus/{uuid}", uuid).retrieve()
+                .bodyToMono(Status.class)
+                .retryWhen(Retry.backoff(  10, Duration.ofSeconds(1)));
     }
 }
